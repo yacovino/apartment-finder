62139535e521d3e2d7ec5b505612756ab1369fd2
Vik Paruchuri <vik.paruchuri@gmail.com>
2016-03-15 22:52:19 -0700

    Fixups

--- setup_scraper.sh
+++ setup_scraper.sh
@@ -2,9 +2,10 @@
 
 cd ~/cl-rental
 
+sudo apt-get update
 sudo apt-get install python3-pip -y
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
-libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev zip -y
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev zip git-core -y
 sudo pip3 install -r requirements.txt
 
 sudo service scraper stop || true
