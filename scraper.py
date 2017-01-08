1dea5d1b7ca410f4a971d4599d04ffe2fcb7cfab
yacovino <daniel.yacovino@gmail.com>
2017-01-08 14:15:36 -0500

    Updated to include bedrooms

--- scraper.py
+++ scraper.py
@@ -32,6 +32,7 @@ class Listing(Base):
     cl_id = Column(Integer, unique=True)
     area = Column(String)
     bart_stop = Column(String)
+    bedrooms = Column(Integer)
 
 Base.metadata.create_all(engine)
 
@@ -45,7 +46,7 @@ def scrape_area(area):
     :return: A list of results.
     """
     cl_h = CraigslistHousing(site=settings.CRAIGSLIST_SITE, area=area, category=settings.CRAIGSLIST_HOUSING_SECTION,
-                             filters={'max_price': settings.MAX_PRICE, "min_price": settings.MIN_PRICE})
+                             filters={'max_price': settings.MAX_PRICE, "min_price": settings.MIN_PRICE, 'bedrooms': settings.BEDROOMS})
 
     results = []
     gen = cl_h.get_results(sort_by='newest', geotagged=True, limit=20)
@@ -96,7 +97,8 @@ def scrape_area(area):
                 location=result["where"],
                 cl_id=result["id"],
                 area=result["area"],
-                bart_stop=result["bart"]
+                bart_stop=result["bart"],
+                bedrooms=result["bedrooms"]
             )
 
             # Save the listing so we don't grab it again.
