3845502275c03ff0a402441ae31e996f207d5db3
yacovino <daniel.yacovino@gmail.com>
2017-08-30 12:01:05 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -46,7 +46,7 @@ def scrape_area(area):
     :return: A list of results.
     """
     cl_h = CraigslistHousing(site=settings.CRAIGSLIST_SITE, area=area, category=settings.CRAIGSLIST_HOUSING_SECTION,
-                             filters={'max_price': settings.MAX_PRICE, 'min_price': settings.MIN_PRICE, 'bedrooms': 1})
+                             filters={'max_price': settings.MAX_PRICE, 'min_price': settings.MIN_PRICE, 'bedrooms': settings.BEDROOMS})
 
     results = []
     gen = cl_h.get_results(sort_by='newest', geotagged=True, limit=20)
